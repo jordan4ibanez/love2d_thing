b420dad8ca4b726fc31700d6ca6be912ae2dde4a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-09 04:27:26 -0400

    The ball is rolling

--- src/main.lua
+++ src/main.lua
@@ -1,15 +1,30 @@
 love.load = function()
     print("loaded")
 end
-love.quit = function()
-    print("thanks for playing.")
-    return false
-end
 love.update = function(delta)
-end
-love.draw = function()
-    love.graphics.print("hi", 1, 1)
     if love.keyboard.isDown("escape") then
         love.event.quit()
     end
+end
+love.draw = function()
+    love.graphics.push()
+    love.graphics.translate(
+        math.random() * 100,
+        math.random() * 100
+    )
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.print("hi", 1, 1)
+    love.graphics.setColor(255, 255, 0)
+    love.graphics.rectangle(
+        "line",
+        100,
+        100,
+        20,
+        20
+    )
+    love.graphics.pop()
+end
+love.quit = function()
+    print("thanks for playing.")
+    return false
 end
--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,27 @@ love.load = (): void => {
 
 love.update = (delta: number) => {
   // print(delta)
-}
-
-love.draw = (): void => {
-  love.graphics.print("hi", 1, 1)
-
   if (love.keyboard.isDown("escape")) {
     love.event.quit()
   }
+}
+
+love.draw = (): void => {
+  love.graphics.push()
+
+  love.graphics.translate(math.random() * 100, math.random() * 100)
+
+  love.graphics.setColor(255, 255, 255)
+
+  love.graphics.print("hi", 1, 1)
+
+  love.graphics.setColor(255, 255, 0)
+
+  love.graphics.rectangle("line", 100, 100, 20, 20)
+
+  love.graphics.pop()
+
+
 }
 
 love.quit = (): boolean => {
