2d1d4529fce34b85bb034c8e8ccc990c116529fd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-09 04:08:25 -0400

    This horrendous deps installer

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,1 +1,13 @@
-npm install -D typescript typescript-to-lua lua-types love-typescript-definitions
+# This script ends up making weird issues if done with -D (local) only instead of combining with -g (global)
+# todo: sort this mess out, or don't.
+sudo npm install -g typescript
+sudo npm install -g typescript-to-lua
+sudo npm install -g lua-types
+sudo npm install -g love-typescript-definitions
+sudo npm install -g @typescript-to-lua/language-extensions
+
+sudo npm install -D typescript
+sudo npm install -D typescript-to-lua
+sudo npm install -D lua-types
+sudo npm install -D love-typescript-definitions
+sudo npm install -D @typescript-to-lua/language-extensions
